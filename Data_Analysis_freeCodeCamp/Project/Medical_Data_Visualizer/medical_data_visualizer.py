ecaef6f19d9a4032f0b2945d54a7ea546cf998c2
Md Imam Ahasan <emamahasane@gmail.com>
2021-12-04 04:00:05 +0600

    Update medical_data_visualizer.py

--- Data_Analysis_freeCodeCamp/Project/Medical_Data_Visualizer/medical_data_visualizer.py
+++ Data_Analysis_freeCodeCamp/Project/Medical_Data_Visualizer/medical_data_visualizer.py
@@ -3,22 +3,17 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Import data
 df = pd.read_csv('medical_examination.csv')
 
-# Add 'overweight' column
 df['overweight'] = df['weight']/((df['height']/100)**2)
 df.loc[df['overweight'] <= 25, 'overweight'] = 0
 df.loc[df['overweight'] > 25, 'overweight'] = 1
 
-# Normalize data by making 0 always good and 1 always bad. If the value of 'cholestorol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 df.loc[df['gluc'] == 1, 'gluc'] = 0
 df.loc[df['gluc'] > 1, 'gluc'] = 1
 df.loc[df['cholesterol'] == 1, 'cholesterol'] = 0
 df.loc[df['cholesterol'] > 1, 'cholesterol'] = 1
 
-
-# Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
     df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
@@ -36,8 +31,6 @@ def draw_cat_plot():
     fig.savefig('catplot.png')
     return fig
 
-
-# Draw Heat Map
 def draw_heat_map():
     # Clean the data
     height025 = df['height'].quantile(0.025)
@@ -50,21 +43,16 @@ def draw_heat_map():
     df_heat = df_heat[df_heat['weight'] >= weight025]
     df_heat = df_heat[df_heat['weight'] <= weight975]
 
-    # Calculate the correlation matrix
     corr = df_heat.corr()
     corr = corr.round(1)
 
-    # Generate a mask for the upper triangle
     mask = np.triu(np.ones_like(corr, dtype=np.bool))
 
-    # Set up the matplotlib figure
     fig, ax = plt.subplots(figsize=(12, 9))
 
-    # Draw the heatmap with 'sns.heatmap()'
     heat_map = sns.heatmap(corr, mask=mask, annot=True, fmt=".1f", square=True)
     heat_map.set_yticklabels(heat_map.get_yticklabels(), rotation=0)
     heat_map.set_xticklabels(heat_map.get_xticklabels(), rotation=90)
 
-    # Do not modify the next two lines
     fig.savefig('heatmap.png')
     return fig
